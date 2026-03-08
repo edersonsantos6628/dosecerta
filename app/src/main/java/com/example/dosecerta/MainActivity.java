92c2e9dbf952f95a32b0152f26b584f523300c27
Ederson silva <edersonsantos628@gmail.com>
2026-03-08 12:55:32 -0300

    Mudança no java

--- app/src/main/java/com/example/dosecerta/MainActivity.java
+++ app/src/main/java/com/example/dosecerta/MainActivity.java
@@ -1,11 +1,18 @@
 package com.example.dosecerta;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.ResourceManagerInternal;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -15,7 +22,7 @@ public
 class MainActivity extends AppCompatActivity implements View
 
         .OnClickListener {
-
+    ImageView foto;
     Button btAddFoto, btSalvar;
 
     @Override
@@ -28,10 +35,60 @@ class MainActivity extends AppCompatActivity implements View
         btAddFoto.setOnClickListener(this);
         btSalvar.setOnClickListener(this);
 
+        foto = findViewById(R.id.foto);
+
     }
 
     @Override
-    public void onClick(View v){
-
+    public void onClick(View v) {
+        if (v.getId() == R.id.addfoto) {
+            escolherImagem();
+        }
     }
-}
+
+    private void escolherImagem() {
+        String[] opcoes = {"Camera", "Galeria"};
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setItems(opcoes, (dialog, which) -> {
+            if (which == 0) {
+                abrirCamera();
+            }
+            if (which == 1) {
+                abrirGaleria();
+            }
+        });
+        builder.show();
+    }
+
+    private void abrirCamera() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, 1);
+    }
+
+    private void abrirGaleria() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent, 2);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            if (requestCode == 1) {
+
+                Bundle extras = data.getExtras();
+                Bitmap fotoBitmap = (Bitmap) extras.get("data");
+                foto.setImageBitmap(fotoBitmap);
+
+            }
+            }
+            if (requestCode == 2) {
+                Uri imagem = data.getData();
+                foto.setImageURI(imagem);
+
+            }
+
+        }
+    }
